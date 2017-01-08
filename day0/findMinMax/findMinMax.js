a7e0bff2ac81bf190b4581ab057828cda98ac935
adenmosun <adenmosun@users.noreply.github.com>
2017-01-08 14:19:50 +0000

    Update findMinMax.js

--- day0/findMinMax/findMinMax.js
+++ day0/findMinMax/findMinMax.js
@@ -4,18 +4,20 @@ var file = {}
 
     file.findMinMax = function (arr) {
 	// TODO
-		var Min = Math.min.apply(null, arr);
-		var Max = Math.max.apply(null, arr);
-		var MinMax = [Min,Max]
-		var	equal = []
-		
-		for(var i = 0; i < arr.length; i++){
-			if(arr[i] == arr[i+1]){
-				 equal.push(arr[i])
-				return equal
-			}
-			else return MinMax
-		}
-	}
+	var min = arr[0];
+	var max = arr[0];
+	
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+  }
+        
+        if (min === max) 
+      	  return [min];
+        else
+	  return [min, max];
+}
+
+
 
 module.exports = file;
