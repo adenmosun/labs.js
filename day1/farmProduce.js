1c07a8045656e3fd13e103ab751c60702fd5d941
adenmosun <adenmosun@users.noreply.github.com>
2017-01-09 14:27:56 +0000

    Update farmProduce.js

--- day1/farmProduce.js
+++ day1/farmProduce.js
@@ -5,9 +5,9 @@ var file = {}
 file.farmProduce = function (name, type) {
   this.name = ((typeof (name) === 'undefined') ? 'produce' : name);
   this.type = ((typeof (type) === 'undefined') ? 'food' : type);
-  this.quality = ((name === 'beans') ? 'dry' : 'fresh'); //the quality is either fresh or dry if beans
-  this.harvest = ((type === 'fruits') ? 2 : 1); // the harvest is either within two years(type - fruits) or one year for others
-  this.isHealthy = ((name === 'yam') ? false:  true); // yam is not healthy everything else is
+  this.quality = ((name === 'beans') ? 'dry' : 'fresh'); //the quality is either fresh or dry if beans.
+  this.harvest = ((type === 'fruits') ? 2 : 1); // the harvest is either within two years(type - fruits) or one year for others.
+  this.isHealthy = ((name === 'yam') ? false:  true); // yam is not healthy everything else is.
   this.weight = '0 kg';
   this.cost = function (price) {
     this.total = (type === 'fruits') ? (price * 2) + ' /kg' : (price * 5) + ' /kg'; // fruits cost 2*price/kg everything else costs 5*price/kg
