eacbf02f279bee767378e690d3d44475a69f58b5
adenmosun <adenmosun@users.noreply.github.com>
2017-01-09 14:41:20 +0000

    Update getPrimes.js

--- day1/getPrimes.js
+++ day1/getPrimes.js
@@ -11,12 +11,13 @@ file.getPrimes = function (num) {
         // for every number within the range of the current loop of j from 1 to num it is prime only if 1 and num can be divided without a remainder
 }
 }
-        if(count === 2){ isPrime.push(j); 
+         if(count === 2){ isPrime.push(j); 
         // prime numbers should only be divided by 1 and itself which makes it the count (2)
 }
-  count = 0;
+   count = 0;
 }
 return isPrime;
-}
-// Asymptotic analysis is n^2 because of the nested loops i.e.  O(n^2)
+}// Asymptotic analysis is n^2 because of the nested loops i.e.  O(n^2)
+
+
 module.exports = file;
